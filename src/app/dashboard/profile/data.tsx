a50165076573b20eb85cc571ea6cd4f607a7e200
sunshine-web-coder <webdev4gaming@gmail.com>
2024-04-25 14:39:59 +0100

    profile page updated

--- src/app/dashboard/profile/data.tsx
+++ src/app/dashboard/profile/data.tsx
@@ -1,12 +1,12 @@
-import web3 from "../../../assets/aalogo/web3.png"
-import deposit from "../../../assets/aalogo/deposit.png"
-import ref from "../../../assets/aalogo/ref.png"
-import p2p from "../../../assets/aalogo/p2p.png"
-import square from "../../../assets/aalogo/square.png"
-import earn from "../../../assets/aalogo/earn.png"
-import copyTrade from "../../../assets/aalogo/copyTrade.png"
-import spot from "../../../assets/aalogo/spot.png"
-import future from "../../../assets/aalogo/future.png"
+// import web3 from "../../../assets/aalogo/web3.png"
+// import deposit from "../../../assets/aalogo/deposit.png"
+// import ref from "../../../assets/aalogo/ref.png"
+// import p2p from "../../../assets/aalogo/p2p.png"
+// import square from "../../../assets/aalogo/square.png"
+// import earn from "../../../assets/aalogo/earn.png"
+// import copyTrade from "../../../assets/aalogo/copyTrade.png"
+// import spot from "../../../assets/aalogo/spot.png"
+// import future from "../../../assets/aalogo/future.png"
 
 export const shortcut = [
   {
